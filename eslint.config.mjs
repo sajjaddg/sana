28f18c83d9b87cdb0c144a78e4cd4caf2d6c918c
sajjad hayati <sajjaddg@gmail.com>
2024-12-26 15:56:32 +0330

    style: init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,64 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "node:path"
+import { fileURLToPath } from "node:url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from "@eslint/eslintrc"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:unicorn/recommended",
+    "plugin:import/recommended",
+    "plugin:playwright/recommended",
+    "plugin:prettier/recommended",
+  ),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+      "unicorn/no-array-callback-reference": "off",
+      "unicorn/no-array-for-each": "off",
+      "unicorn/no-array-reduce": "off",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          allowList: {
+            e2e: true,
+          },
+          replacements: {
+            props: false,
+            ref: false,
+            params: false,
+          },
+        },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
+  {
+    files: ["*.js"],
+    rules: {
+      "unicorn/prefer-module": "off",
+    },
+  },
+  {
+    ignores: [".next/", "node_modules/", "next-env.d.ts", "eslint.config.mjs"],
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
   content: [
@@ -11,8 +11,30 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "grey-classic": {
+          50: "var(--grey-classic-50)",
+          100: "var(--grey-classic-100)",
+          200: "var(--grey-classic-200)",
+          300: "var(--grey-classic-300)",
+          400: "var(--grey-classic-400)",
+          500: "var(--grey-classic-500)",
+          600: "var(--grey-classic-600)",
+          700: "var(--grey-classic-700)",
+          800: "var(--grey-classic-800)",
+          900: "var(--grey-classic-900)",
+        },
+      },
+      fontSize: {
+        "8": ["var(--text-8-font-size)", "var(--text-8-line-height)"],
+        "7": ["var(--text-7-font-size)", "var(--text-7-line-height)"],
+        "6": ["var(--text-6-font-size)", "var(--text-6-line-height)"],
+        "5": ["var(--text-5-font-size)", "var(--text-5-line-height)"],
+        "4": ["var(--text-4-font-size)", "var(--text-4-line-height)"],
+        "3": ["var(--text-3-font-size)", "var(--text-3-line-height)"],
+        "2": ["var(--text-2-font-size)", "var(--text-2-line-height)"],
+        "1": ["var(--text-1-font-size)", "var(--text-1-line-height)"],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
